355fa49dd4f1c6ee5927f343f74018ca7f4b83a4
sadeem <sadeemalharbi21@example.com>
2026-05-12 11:56:46 +0300

    Base Folder

--- Autism/main/views.py
+++ Autism/main/views.py
@@ -5,4 +5,4 @@ from django.http import HttpRequest, HttpResponse
 
 def home_page_view(request:HttpRequest):
 
-    return render(request, 'main/home_page.html')
+    return render(request, 'main/base.html')
