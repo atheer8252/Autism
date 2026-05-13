b56af70d5cea8c384a999755cfa99f44908e7100
Atheer <utherr120@gmail.com>
2026-05-13 10:12:12 +0300

    home_bag

--- Autism/main/urls.py
+++ Autism/main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name="main"
+app_name = "main"
 
-urlpatterns=[
-    path('', views.home_page_view, name="home_page_view"),
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about-us/", views.about_us, name="about_us"),
+    path("contact-us/", views.contact_us, name="contact_us"),
+    path("how-it-works/", views.how_it_works, name="how_it_works"),
+    path("privacy-policy/", views.privacy_policy, name="privacy_policy"),
+    path("terms-of-service/", views.terms_of_service, name="terms_of_service"),
+    path("cookie-policy/", views.cookie_policy, name="cookie_policy"),
 ]
--- Autism/main/views.py
+++ Autism/main/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
 
-def home_page_view(request:HttpRequest):
-
+def home_page_view(request: HttpRequest):
     return render(request, 'main/home_page.html')
+
+
+def about_us_view(request: HttpRequest):
+    return render(request, 'main/about-us.html')
+
+
+def contact_us_view(request: HttpRequest):
+    return render(request, 'main/contact-us.html')
+
+
+def how_it_works_view(request: HttpRequest):
+    return render(request, 'main/how-it-works.html')
+
+
+def privacy_policy_view(request: HttpRequest):
+    return render(request, 'main/privacy-policy.html')
+
+
+def terms_of_service_view(request: HttpRequest):
+    return render(request, 'main/terms-of-service.html')
+
+
+def cookie_policy_view(request: HttpRequest):
+    return render(request, 'main/cookie-policy.html')
