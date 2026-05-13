5a1448b03b65f2eee2f1a5a2fc7a00c6eaaa789f
sadeem <sadeemalharbi21@example.com>
2026-05-13 08:15:26 +0300

    fix(assessment):update HTML structure and CSS for upload_video.html

--- Autism/Autism/settings.py
+++ Autism/Autism/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'cloudinary_storage',
     'main',
+    'assessment',
 ]
 
 MIDDLEWARE = [
---
+++ Autism/assessment/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render 
+from django.http import HttpRequest, HttpResponse
+
+
+# Create your views here.
+
+def upload_video (request:HttpRequest):
+    return render(request, 'assessment/upload_video.html')
