e4950046299bec970f9c343365539bd7f25c12d9
Norah Saeed <norahseedw@gmail.com>
2026-05-15 17:21:34 +0300

    add models community

--- Autism/community/models.py
+++ Autism/community/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('sleep', 'النوم'),
+        ('food', 'التغذية'),
+        ('play', 'اللعب'),
+        ('education', 'التعليم'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES)
+
+    content = models.TextField()
+
+    tags = models.CharField(max_length=200,blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True )
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Like(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['post', 'user']
--- Autism/community/urls.py
+++ Autism/community/urls.py
@@ -6,5 +6,10 @@ app_name="community"
 urlpatterns=[
     path('community/' , views.community_view, name='community_view'),
     path('create_post/', views.create_post_view, name='create_post_view'),
-    path('details_post/', views.details_post_view, name='details_post_view'),
+    path('details_post/<int:post_id>/', views.details_post_view, name='details_post_view'),
+    path('like_post/<int:post_id>/', views.like_post_view, name='like_post_view'),
+    path('edit_post/<int:post_id>/',views.edit_post_view,name='edit_post_view'),
+    path('delete_post/<int:post_id>/',views.delete_post_view,name='delete_post_view'),
+    path('edit_comment/<int:comment_id>/',views.edit_comment_view,name='edit_comment_view'),
+    path('delete_comment/<int:comment_id>/',views.delete_comment_view,name='delete_comment_view'),
 ]
